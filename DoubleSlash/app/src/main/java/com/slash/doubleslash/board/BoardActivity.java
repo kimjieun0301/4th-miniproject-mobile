f75b57e67f83f95f20915892bcada479439a6853
juhyang <ljh28891004@gmail.com>
2020-07-26 14:58:03 +0900

    boardActivity

---
+++ DoubleSlash/app/src/main/java/com/slash/doubleslash/board/BoardActivity.java
@@ -0,0 +1,16 @@
+package com.slash.doubleslash.board;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.slash.doubleslash.R;
+
+public class BoardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_board);
+    }
+}
